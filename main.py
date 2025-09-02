2c5073330c6aff002fbcb6a38ffd227ef7e879ee
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-09-03 00:11:00 +0500

    imp

--- main.py
+++ main.py
@@ -61,7 +61,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://admin.lruss.uz",
+        "https://lruss.uz"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
